acd10706f2b97775d290ed383fa5ee7c41d3f47f
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2019-09-13 10:47:12 -0400

    rss feed implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-feed`,
         {
             resolve: `gatsby-source-contentful`,
             options: {
