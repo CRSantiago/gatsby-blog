3987798cd209bdbb4285abd1280f2fd6e9d878d6
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2019-09-18 16:33:24 -0400

    form error

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -7,7 +7,7 @@ const SubscribePage = () => {
   return(
     <Layout>
       <Head title="Subscribe"/>
-      <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" className={subscribeStyles.myForm}>
+      <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" action="/"className={subscribeStyles.myForm}>
         <p>
           <label>First Name: <input type="text" name="firstname" placeholder="John"/></label>   
         </p>
